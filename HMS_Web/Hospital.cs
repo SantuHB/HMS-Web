2d8612218c5f06526d487cb4e65fd9c67b9f452b
SanthoshHB <santhosh.proxy@gmail.com>
2024-07-24 17:38:40 +0530

    Files Committed-1

---
+++ HMS_Web/Hospital.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS_Web
+{
+    internal class Hospital
+    {
+        public void PrintHospitalName()
+        {
+            Console.WriteLine("E-Corl HMS");
+        }
+    }
+}
